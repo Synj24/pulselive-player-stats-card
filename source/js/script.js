7b71538cc000f237e393572e0fbd7337ad975720
Matthew Gregory <synj@Matthews-iMac.local>
2020-12-26 22:17:44 +0000

    wrote function to fetch data

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,26 @@
-let message = "Hello World!"
+let playerData = '';
 
-console.log(message);
+getData("../../data/player-stats.json").then(data => {
+    playerData = data;
+    console.log(playerData[0].player.name.first + " " + playerData[0].player.name.last);
+    console.log(
+        decodePosition(playerData[0].player.info.position));
+})
+
+// Generic function to getData from a url
+function getData(url) {
+    return new Promise((resolve, reject) => {
+        fetch(url)
+            .then((response) => {
+                return response.json();
+            }).then((data) => {
+                data = data.players;
+                resolve(data);
+            })
+    })
+};
+
+function decodePosition(code) {
+    if (code === "D")
+        return "Defender";
+}
